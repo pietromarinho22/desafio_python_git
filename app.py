d7d3a35e9a3a5967ad1972d8856c486d46ca93a5
pietromarinho22 <pietrovmarinho@gmail.com>
2025-09-08 13:22:51 -0300

    inicial

---
+++ app.py
@@ -0,0 +1,4 @@
+nome = input ('Escreva seu nome')
+idade = int (input('Insira seu idade'))
+linguagemFavorita = input ('Coloque a sua linguagem favorita')
+print = (f'O seu nome, idade e linguagem de programação favorita é {nome}, {idade} e {linguagemFavorita}')
